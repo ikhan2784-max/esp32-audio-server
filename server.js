f3eb56a72e5db4d0a843ae48266d369e05a55a3d
Syed Najamuddin Faisal <imran5924@gmail.com>
2026-02-28 11:03:18 -0500

    Initial push with ES module fix

---
+++ server.js
@@ -0,0 +1,50 @@
+import http from "http";
+import { WebSocketServer } from "ws";
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end("ESP32 Audio Server Running");
+});
+
+const wss = new WebSocketServer({ server });
+
+let espSocket = null;
+let listenerSocket = null;
+
+wss.on("connection", (ws, request) => {
+  const url = new URL(request.url, `http://${request.headers.host}`);
+  const type = url.searchParams.get("type");
+
+  console.log("New connection:", type);
+
+  if (type === "esp") {
+    espSocket = ws;
+    console.log("ESP connected");
+
+    ws.on("message", (data) => {
+      if (listenerSocket && listenerSocket.readyState === 1) {
+        listenerSocket.send(data);
+      }
+    });
+
+    ws.on("close", () => {
+      console.log("ESP disconnected");
+      espSocket = null;
+    });
+  }
+
+  if (type === "listen") {
+    listenerSocket = ws;
+    console.log("Listener connected");
+
+    ws.on("close", () => {
+      console.log("Listener disconnected");
+      listenerSocket = null;
+    });
+  }
+});
+
+const PORT = process.env.PORT || 10000;
+server.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
